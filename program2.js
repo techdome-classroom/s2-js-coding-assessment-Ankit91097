10bbd26721b406bf5b6ba575165a5029801e5091
Ankit Niranjan <iambot331@gmail.com>
2024-09-27 14:10:18 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -15,7 +15,7 @@ var romanToInt = function(s) {
     let total=0;
     for(let i=0;i<s.length;i++){
         const currentVal=romanNumerals[s[i]];
-        
+        const nextVal=romanNumerals[s[i+1]]
     }
 };
 
